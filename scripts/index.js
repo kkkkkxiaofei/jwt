8be2874dfa6f6308a493fadd1e4062709a05ad08
pursue <348564768@qq.com>
2021-12-20 22:09:40 +0800

    add raw verify

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
-const { decode, sign } = require('../utils');
+const fs = require('fs');
+const path = require('path');
+const { decode, sign, verify, rawVerify } = require('../utils');
 const { ID_TOKEN } = require('../config');
 
-const payload = decode(ID_TOKEN);
-const idToken = sign(payload);
 
-console.log(payload);
-console.log(payload === idToken);
+const decodedIdToken = decode(ID_TOKEN, { complete: true });
+// const idToken = sign(decodedIdToken.payload);
+
+// console.log(decodedIdToken);
+// console.log(ID_TOKEN === idToken);
+// console.log(decode(idToken, { complete: true }));
+
+const pem = fs.readFileSync(path.resolve('./public.pem'));
+// console.log(Buffer.from(pem, 'base64').toString());
+// console.log(verify(ID_TOKEN, pem));
+
+
+console.log(rawVerify(ID_TOKEN, pem));
--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,35 @@
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const { SECRET } = require('../config');
 
-const decode = value => jwt.decode(value);
+const decode = (value, options = {}) => jwt.decode(value, options);
 
-const sign = (payload, serectOrPrivateKey = SECRET) => jwt.sign(payload, serectOrPrivateKey);
+const sign = (payload, serectOrPrivateKey = SECRET) => 
+  jwt.sign(payload, serectOrPrivateKey, {algorithm: 'RS256'});
+
+const verify = (token, secretOrPublickKey) => new Promise((resolve, reject) => {
+  jwt.verify(token, secretOrPublickKey, {
+    ignoreExpiration: true, 
+  }, (error, decoded) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(decoded);
+    }
+  });
+});
+
+const rawVerify = (jws, secretOrPublickKey) => {
+  const thing = jws.split('.', 2).join('.');
+  const signature = jws.split('.')[2];
+  var verifier = crypto.createVerify('RSA-SHA256');
+  verifier.update(thing);
+  return verifier.verify(secretOrPublickKey, signature, 'base64');
+};
 
 module.exports = {
   decode,
-  sign
+  sign,
+  verify,
+  rawVerify,
 };
